8b632c98686482dc764abeb038795675213bcc21
Anthony Nash <anthony.nash@du.co>
2021-09-05 23:42:07 +0100

    _Many_ UI updates

--- data/web/js/script.js
+++ data/web/js/script.js
@@ -1,4 +1,4 @@
-function getInputType(value) {
+function get_input_type(value) {
   input = {
     column: {
       classes: "col-9 ignore-screen level-item",
@@ -24,7 +24,7 @@ function getInputType(value) {
   return input;
 }
 
-async function saveData(url, params) {
+async function save_data(url, params) {
   const data = new URLSearchParams();
   Object.entries(params).forEach((param) => {
     const [key, val] = param;
@@ -39,7 +39,7 @@ async function saveData(url, params) {
   });
 }
 
-async function showSnackbar(el, page, res) {
+async function show_snackbar(el, page, res) {
   if (res.status === 200) {
     el.innerText = `${page} successfully saved`;
     el.classList.add("success");
@@ -55,151 +55,191 @@ async function showSnackbar(el, page, res) {
   }, 2000);
 }
 
-function loadData() {
+function load() {
   return {
-    tab: "logging",
+    tab: 'logging',
     $refs: null,
     logging: {
       wrap_lines: false,
     },
-    settings: {
-      $refs: null,
-      url: "/api/settings",
-      data: {},
-      tab: "all",
-      async refresh() {
-        res = await fetch(this.url);
-        data = await res.json();
-        this.data = data;
-      },
-      async save() {
-        params = { settings: JSON.stringify(this.data) };
-        res = await saveData(this.url, params);
-        showSnackbar(this.$refs.snackbar, "Settings", res);
-        this.refresh();
-      },
-      getInputType(value) {
-        return getInputType(value);
-      },
-    },
-    devices: {
-      $refs: null,
-      url: "/api/devices",
-      data: {},
-      editting: {
-        name: "",
-        type: "",
-        uuid: "",
-        index: null,
-      },
-      modal: null,
-      tab: "all",
-      types: [],
-      async refresh() {
-        res = await fetch(this.url);
-        data = await res.json();
-        this.types = [...new Set(data.map((d) => d["type"]))];
-        this.data = data;
-      },
-      async save() {
-        params = { devices: JSON.stringify(this.data) };
-        res = await saveData(this.url, params);
-        showSnackbar(this.$refs.snackbar, "Devices", res);
-        this.refresh();
-      },
-      add() {
-        this.editting.index = this.data.length;
-        this.modal.classList.add("shown");
-      },
-      delete(i) {
-        this.data.splice(i, 1);
-      },
-      edit(i) {
-        device = JSON.parse(JSON.stringify(this.data[i]));
-        device.index = i;
-        this.editting = device;
-        this.modal.classList.add("shown");
-      },
-      resetEditting() {
-        this.modal.classList.remove("shown");
-        this.editting = {
-          name: "",
-          type: "",
-          uuid: "",
-          index: null,
-        };
-      },
-      saveEditting() {
-        i = this.editting.index;
-        delete this.editting.index;
-        this.data[i] = this.editting;
-        this.resetEditting();
-        this.save();
-      },
-      validateEditting() {
-        e = this.editting;
-        return e.name.length > 0 && e.uuid.length > 0;
-      },
-    },
+    logging: load_logging(),
+    devices: load_devices(),
+    settings: load_settings(),
     isLoaded: false,
-    async getPartial(partial) {
+    async init() {
+      this.logging.start_socket();
+      await this.devices.refresh();
+      await this.settings.refresh();
+      this.isLoaded = true;
+    },
+    async get_partial(partial) {
       url = `/partials/${partial}.html`;
       res = await fetch(url);
       text = await res.text();
       return text;
     },
-    propagate(refs) {
-      this.$refs = refs;
-      this.devices.$refs = refs;
-      this.settings.$refs = refs;
-    },
-    toggleMenu() {
+    toggle_menu() {
       this.$refs.menu_button.classList.toggle("active");
       this.$refs.menu.classList.toggle("active");
     },
-    setActiveTab(tab) {
+    set_active_tab(tab) {
       this.tab = tab;
-      this.toggleMenu();
+      this.toggle_menu();
     },
-    getTabClasses(tab) {
+    get_tab_classes(tab) {
       return (
         "col-xs-12 col-md-10 col-lg-8 offset-center p-0" +
         (this.tab == tab ? "" : " u-none")
       );
     },
-    async init() {
-      await this.settings.refresh();
-      await this.devices.refresh();
-      this.isLoaded = true;
+    async reboot() {
+      await fetch("/api/reboot");
+      while (true) {
+        await fetch("/api/status");
+        if (res.status === 200) {
+          break;
+        }
+      }
+      location.reload();
+    },
+    async clearIgnores() {
+      fetch("/api/clearignores");
     },
   };
 }
 
-function startSocket() {
+function load_settings() {
+  return {
+    url: "/api/settings",
+    data: {},
+    $refs: null,
+    tab: "network",
+    dirty: false,
+    async refresh() {
+      res = await fetch(this.url);
+      data = await res.json();
+      this.data = data;
+    },
+    async save() {
+      params = { settings: JSON.stringify(this.data) };
+      res = await save_data(this.url, params);
+      if (res.status === 200) {
+        this.dirty = false;
+      }
+      show_snackbar(this.$refs.snackbar, "Settings", res);
+    },
+    get_input_type(value) {
+      return get_input_type(value);
+    },
+  };
+}
+
+function load_devices() {
+  return {
+    url: "/api/devices",
+    data: [],
+    editting: {
+      name: "",
+      type: "",
+      uuid: "",
+      index: null,
+    },
+    modal: null,
+    $refs: null,
+    tab: "all",
+    types: [],
+    inputs: {
+      uuid: {
+        pattern: "",
+        title: ""
+      }
+    },
+    async refresh() {
+      res = await fetch(this.url);
+      data = await res.json();
+      this.types = [...new Set(data.map((d) => d["type"]))];
+      this.data = data;
+    },
+    async save() {
+      params = { devices: JSON.stringify(this.data) };
+      res = await save_data(this.url, params);
+      show_snackbar(this.$refs.snackbar, "Devices", res);
+      this.refresh();
+    },
+    add() {
+      this.editting.index = this.data.length;
+      this.$refs.device_modal.classList.add("shown");
+    },
+    async delete(i) {
+      this.data.splice(i, 1);
+      await this.save();
+    },
+    edit(i) {
+      device = JSON.parse(JSON.stringify(this.data[i]));
+      device.index = i;
+      this.editting = device;
+      this.$refs.device_modal.classList.add("shown");
+    },
+    set_id_pattern(type) {
+      switch (type) {
+        case 'Beacon':
+          this.inputs.uuid.pattern = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}';
+          this.inputs.uuid.title = 'Uuid should be in the format abcdef01-2345-6789-abcd-ef0123456789';
+          break;
+        default:
+          this.inputs.uuid.pattern = ".*"
+          this.inputs.uuid.title = ""
+      }
+    },
+    reset_editting() {
+      this.$refs.device_modal.classList.remove("shown");
+      this.editting = {
+        name: "",
+        type: "",
+        uuid: "",
+        index: null,
+      };
+    },
+    save_editting() {
+      new_dev = this.editting;
+      i = new_dev.index;
+      delete new_dev.index;
+      this.data[i] = new_dev;
+      this.reset_editting();
+      this.save();
+    },
+    validate_editting() {
+      e = this.editting;
+      return e.name.length > 0 && e.uuid.length > 0;
+    },
+  };
+}
+
+function load_logging() {
   return {
     log_lines: [],
     state: false,
     state_message: "Socket disconnected",
-    init() {
-      log_lines =  this.log_lines;
+    async start_socket() {
+      log_lines = this.log_lines;
       state = this.state;
       state_message = this.state_message;
       let socket = new WebSocket(`ws://${window.location.hostname}/ws`);
-      socket.onopen = function(e) {
+      socket.onopen = function (e) {
         state = true;
         state_message = "Socket connected";
       };
-      socket.onmessage = function(e) {
+      socket.onmessage = function (e) {
         log_lines.unshift(e.data);
       };
-      socket.onclose = function(e) {
+      socket.onclose = function (e) {
         state_message = "Socket closed";
         state = false;
       };
-      socket.onerror = function(e) {
+      socket.onerror = function (e) {
         state_message = `Socket error: ${e.message}`;
         state = false;
-      }
+      };
     },
   };
 }
